cf9f25effde2be4b049e488f4f5ad8c95e242f5e
luanjintai <tylerluan@163.com>
2022-03-10 14:31:10 +0800

    try

--- setup.py
+++ setup.py
@@ -5,6 +5,6 @@ setuptools.setup(
     version="0.2.1",
     author="ykk648",
     description="cv toolbox - git easy install version",
-    packages=setuptools.find_packages(),
+    packages=['./'],
     python_requires='>=3.6',
 )
